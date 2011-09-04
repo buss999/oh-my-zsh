2e07a5995a50b4e97aa1e4061c75019acdc5f9c4
William Pettersson <william.pettersson@gmail.com>
2011-09-04 15:14:47 +1000

    Fix home/end keys

--- lib/key-bindings.zsh
+++ lib/key-bindings.zsh
@@ -15,8 +15,10 @@ bindkey '^p' up-line-or-search
 bindkey '^n' down-line-or-search
 
 bindkey "^[[H" beginning-of-line
+bindkey "^[OH" beginning-of-line
 bindkey "^[[1~" beginning-of-line
 bindkey "^[[F"  end-of-line
+bindkey "^[OF"  end-of-line
 bindkey "^[[4~" end-of-line
 bindkey ' ' magic-space    # also do history expansion on space
 
